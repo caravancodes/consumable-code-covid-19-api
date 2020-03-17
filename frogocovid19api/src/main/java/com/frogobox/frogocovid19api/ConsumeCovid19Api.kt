9a44dba53a672a4206dc8ebb4b60dc37b5dbd42f
amirisback <faisalamircs@gmail.com>
2020-03-17 18:01:18 +0700

    add: ConsumeCovid19Api.kt

---
+++ frogocovid19api/src/main/java/com/frogobox/frogocovid19api/ConsumeCovid19Api.kt
@@ -0,0 +1,22 @@
+package com.frogobox.frogocovid19api
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * Covid19Api
+ * Copyright (C) 17/03/2020.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.frogocovid19api
+ *
+ */
+class ConsumeCovid19Api : ConsumeCovid19ApiView {
+
+}
