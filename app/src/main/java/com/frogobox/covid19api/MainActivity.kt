c13912de550473aef833fa54d92edb31617867f4
amirisback <faisalamircs@gmail.com>
2020-03-18 13:24:41 +0700

    add: testing apps done

--- app/src/main/java/com/frogobox/covid19api/MainActivity.kt
+++ app/src/main/java/com/frogobox/covid19api/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.frogobox.covid19api
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.frogobox.frogocovid19api.ConsumeCovid19Api
+import com.frogobox.frogocovid19api.callback.Covid19ResultCallback
+import com.frogobox.frogocovid19api.data.reponse.ReponseSummary
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val consumeCovid19Api = ConsumeCovid19Api()
+        consumeCovid19Api.usingChuckInterceptor(this)
+        consumeCovid19Api.getSummaryData(object : Covid19ResultCallback<ReponseSummary> {
+            override fun getResultData(data: ReponseSummary) {
+                println(data.countries?.get(0)?.country)
+            }
+
+            override fun failedResult(statusCode: Int, errorMessage: String?) {
+
+            }
+
+            override fun onShowProgress() {
+
+            }
+
+            override fun onHideProgress() {
+
+            }
+        })
+
     }
 }
